b7e19189f763051b82478b291fa9452ecbf8efdc
SunnyDevZH <yannick.vaterlaus@windowslive.com>
2024-10-03 12:06:04 +0200

    add chat

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,13 @@
+from .models import Chat, Message
 from django.contrib import admin
 
+# Hier kann ich z.B. Filter und Suche erstellen
+class MessageAdmin(admin.ModelAdmin):
+    fields = ('chat','text','created_at', 'author', 'receiver')    
+    list_display = ('created_at', 'author', 'text', 'receiver')    
+    search_fields = ('text',)
+
 # Register your models here.
+
+admin.site.register(Message, MessageAdmin)
+admin.site.register(Chat)
