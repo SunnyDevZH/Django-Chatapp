f59cf0342657660ca15d6b59f01e9bcb8d9fa592
SunnyDevZH <yannick.vaterlaus@windowslive.com>
2024-10-03 16:37:15 +0200

    view

--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from chat.models import Message
 from chat.models import Chat
+from django.http.response import HttpResponseRedirect
+from django.contrib.auth import authenticate, login
 
 def index(request):
     if request.method == 'post':            
@@ -11,3 +13,13 @@ def index(request):
     #chat_messages = Message.objects.filter(chat__id=1)    
     
     return render(request, 'chat/index.html', {'username': 'yannickvaterlaus'})
+
+def login_view(request):
+    if request.method == 'post': 
+        user = authenticate(username=request.Post.get('username'), password=request.Post.get('password'))
+        if user:
+            login(request, user)
+            return HttpResponseRedirect('/chat/')
+        else:
+            return render(request, 'auth/login.html', {'wrongPasswort': True})
+    return render(request, 'auth/login.html')
