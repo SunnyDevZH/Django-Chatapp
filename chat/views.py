0fe26c7ecdc35f56f8ea35cd20ca5ee5083610b5
SunnyDevZH <yannick.vaterlaus@windowslive.com>
2024-10-03 14:45:10 +0200

    view

--- chat/views.py
+++ chat/views.py
@@ -2,13 +2,12 @@ from django.shortcuts import render
 from chat.models import Message
 from chat.models import Chat
 
-def index(request):     
-    if request.method == 'POST':        
-        print("Request method is post")        
-        print(request.POST['textmessage'])        
-        testChat = Chat.objects.get(id=1)        
-        Message.objects.create(text=request.POST['textmessage'], chat=testChat, author=request.user, receiver=request.user)    
+def index(request):
+    if request.method == 'post':            
+        print("Request method is post" + request.POST['textmessage'])        
+        #testChat = Chat.objects.get(id=1)        
+        #Message.objects.create(text=request.POST['textmessage'], chat=testChat, author=request.user, receiver=request.user)    
     
-    chat_messages = Message.objects.filter(chat__id=1)    
+    #chat_messages = Message.objects.filter(chat__id=1)    
     
-    return render(request, 'chat/index.html', {'name': 'yannickvaterlaus', 'chat_messages': chat_messages})
+    return render(request, 'chat/index.html', {'username': 'yannickvaterlaus'})
